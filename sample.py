d9d2e7030876712f58035e8fe5ddd7e272c09d95
Satishdevopslab <144417713+Satishdevopslab@users.noreply.github.com>
2023-09-26 12:23:04 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,9 +1,9 @@
 # This program prints Hello, world!
 
 print('HAI HAI ')
-print('S')
-print('A')
-print('T')
-print('I')
-print('S')
-print('H')
+print('  S')
+print('    A')
+print('      T')
+print('        I')
+print('          S')
+print('            H')
